e144beea36ac11cef4f67815bef01d7ecfae56ce
Martina <martina.pozzerle@libero.it>
2020-10-20 17:13:33 +0200

    #toFixed e calcolo

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var chilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
 var eta = parseInt(prompt("Quanti anni hai?"));
 var costo = 0.21;
+var prezzoPieno = chilometri * costo;
+var scontoUnder = (prezzoPieno - (prezzoPieno * 0.2));
+var scontoOver = (prezzoPieno - (prezzoPieno * 0.4));
 if (isNaN(chilometri) || isNaN(eta)){
   document.getElementById("biglietto").innerHTML = "Impossibile visualizzare biglietto. Non hai inserito dei numeri. Riprova";
 } else {
   if (eta <= 18){
-    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + (((chilometri * costo) * 0.8).toFixed(2)) + " euro";
+    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + ((scontoUnder).toFixed(2)) + " euro";
   } else if (eta >= 65){
-    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + (((chilometri * costo) * 0.6).toFixed(2)) + " euro";
+    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + ((scontoOver).toFixed(2)) + " euro";
   }
   else{
-    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + ((chilometri * costo).toFixed(2)) + " euro";
+    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + (prezzoPieno.toFixed(2)) + " euro";
   }
 }
