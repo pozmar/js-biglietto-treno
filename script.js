b0864c798faa6c73ad94725f09492d806e75c178
Martina <martina.pozzerle@libero.it>
2020-10-20 16:01:35 +0200

    #biglietti costo

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 var chilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
 var eta = parseInt(prompt("Quanti anni hai?"));
 var costo = 0.21;
-var under = eta < 18;
-var over = eta > 65;
-var totaleUnder = (under * costo) * 0.8;
-var totaleOver = (over * costo) * 0.6;
 if (isNaN(chilometri) || isNaN(eta)){
   document.getElementById("biglietto").innerHTML = "Impossibile visualizzare biglietto. Non hai inserito dei numeri. Riprova";
 } else {
-  if (eta < 18){
-    document.getElementById("biglietto").innerHTML = ((chilometri * costo) * 0.8);
-  } else if (eta > 65){
-    document.getElementById("biglietto").innerHTML = ((chilometri * costo) * 0.6);
+  if (eta <= 18){
+    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + ((chilometri * costo) * 0.8 + " euro");
+  } else if (eta >= 65){
+    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + ((chilometri * costo) * 0.6) + " euro";
   }
   else{
-    document.getElementById("biglietto").innerHTML = chilometri * costo;
+    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + (chilometri * costo) + " euro" ;
   }
 }
