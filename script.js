7c1c73d810238c52b493ff01e7b039020d0589a0
Martina <martina.pozzerle@libero.it>
2020-10-21 14:21:07 +0200

    #struttura html e css

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var chilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
-var eta = parseInt(prompt("Quanti anni hai?"));
+//var chilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+//var eta = parseInt(prompt("Quanti anni hai?"));
 var costo = 0.21;
 var prezzoPieno = chilometri * costo;
-var scontoUnder = (prezzoPieno - (prezzoPieno * 0.2));
-var scontoOver = (prezzoPieno - (prezzoPieno * 0.4));
+var scontoUnder = 20;
+var scontoOver = 40;
 if (isNaN(chilometri) || isNaN(eta)){
   document.getElementById("biglietto").innerHTML = "Impossibile visualizzare biglietto. Non hai inserito dei numeri. Riprova";
 } else {
   if (eta <= 18){
-    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + ((scontoUnder).toFixed(2)) + " euro";
+    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + (prezzoPieno - (prezzoPieno * scontoUnder) / 100).toFixed(2) + " euro";
   } else if (eta >= 65){
-    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + ((scontoOver).toFixed(2)) + " euro";
+    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + (prezzoPieno - (prezzoPieno * scontoOver) / 100).toFixed(2) + " euro";
   }
   else{
-    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + (prezzoPieno.toFixed(2)) + " euro";
+    document.getElementById("biglietto").innerHTML = "Il costo del tuo biglietto è " + (prezzoPieno).toFixed(2) + " euro";
   }
 }
